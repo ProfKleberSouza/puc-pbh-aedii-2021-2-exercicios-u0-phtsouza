5fa6160678559eeacf8cdeecef760861daf93d7e
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-07 13:48:34 -0300

    Comentário questão 1

--- exe01/main.c
+++ exe01/main.c
@@ -7,10 +7,12 @@ int main(){
     int num1, num2;
     int soma = 0;
 
+    // printf("Informe um numero");
     scanf("%d", &num1);
+    // printf("Informe outro numero");
     scanf("%d", &num2);
 
-    soma = num1 + num2;
+    soma = num1 + num2; // Realiza a soma dos numeros
 
     printf("SOMA = %d", soma);
 }
