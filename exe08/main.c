268abd9a8252d8cccd711c0ee42669575cc979ec
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-09 11:44:25 -0300

    Exercícios + testes

--- exe08/main.c
+++ exe08/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+/*
+Programa feito para o usuario informar uma sequencia de numeros e após isso informa se o número é positivo
+ou nagativo.
+Entrada: Números quaisquer e 0 para indicar quando você deseja encerrar.
+Saida: Positivo ou negativo de acordo com a ordem que o usuario informou.
+*/
+int main(){
+
+    int n[1000];
+
+    for(int i=0; i<1000; i++){
+        // printf("Informe um numero: ");
+        scanf("%d", &n[i]);
+        if(n[i]==0){
+            break;
+        }
+    }
+
+    for(int i=0; i<1000; i++){
+        if(n[i]>0){
+            printf("\nPOSITIVO");
+        }
+        else if(n[i]<0){
+            printf("\nNEGATIVO");
+        }
+        else if(n[i]==0){
+            break;
+        }
+    }
 }
