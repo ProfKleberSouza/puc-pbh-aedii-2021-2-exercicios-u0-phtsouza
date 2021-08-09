600ae4f41b735f031faad75455e3d60a85d43273
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-09 11:12:25 -0300

    Exercícios + testes

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+/*
+Programa feito para informar o maior e o menor informado pelo usuario.
+Entrada: Quantidade de numeros que deseja informar + os numeros.
+Saida: O maior e o menor número dentre os informados.
+*/
+int main(){
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+    int i;
+    int menor, maior;
+    // printf("Informe quantos numeros deseja passar: ");
+    scanf("%d",&i); // Armazena o tamanho do vetor.
+    int vet[i];
+
+    for(int j=0; j<i;j++){
+        // printf("Informe um numero: ");
+        scanf("%d",&vet[j]);
+    }
+
+    menor = vet[i];
+    maior = vet[0];
+
+    for(int j = 0; j < i; j++){
+        if(vet[j]<menor){
+            menor = vet[j];
+        }
+        if(vet[j]>=maior){
+            maior = vet[j];
+        }
+    }
+    printf("MENOR = %d\n", menor);
+    printf("MAIOR = %d\n", maior);
 }
