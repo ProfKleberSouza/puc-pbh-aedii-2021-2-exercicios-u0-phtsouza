70df4fdfcf9e51c7c53b0c4489b9ebb621700951
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-10 00:00:07 -0300

    Exercícios + testes

--- exe07/main.c
+++ exe07/main.c
@@ -1,54 +1,34 @@
 
-// #include <stdio.h>
-// /*
-// Programa feito para informar o maior e o menor informado pelo usuario.
-// Entrada: Quantidade de numeros que deseja informar + os numeros.
-// Saida: O maior e o menor número dentre os informados.
-// */
-// int main(){
-
-//     int i;
-//     int menor, maior;
-//     // printf("Informe quantos numeros deseja passar: ");
-//     scanf("%d",&i); // Armazena o tamanho do vetor.
-//     int vet[i];
-
-//     for(int j=0; j<i;j++){
-//         // printf("Informe um numero: ");
-//         scanf("%d",&vet[j]);
-//     }
-
-//     menor = vet[i];
-//     maior = vet[0];
-
-//     for(int j = 0; j < i; j++){
-//         if(vet[j]<menor){
-//             menor = vet[j];
-//         }
-//         if(vet[j]>=maior){
-//             maior = vet[j];
-//         }
-//     }
-//     printf("MENOR = %d\n", menor);
-//     printf("MAIOR = %d\n", maior);
-// }
-
 #include <stdio.h>
+/*
+Programa feito para informar o maior e o menor informado pelo usuario.
+Entrada: Quantidade de numeros que deseja informar + os numeros.
+Saida: O maior e o menor número dentre os informados.
+*/
+int main(){
 
-int main() {
-   // ENTRADAS: um numero inteiro N 
-   int N;
-   scanf("%i", &N);
-   int aux; 
-   scanf("%i", &aux); 
-   int maior = aux, menor = aux;
-   for(int i = 0; i < N - 1; i++) {
-      scanf("%i", &aux);
-      aux > maior ? maior = aux : maior; // PROCESSAMENTO
-      aux < menor ? menor = aux : menor; // PROCESSAMENTO
-   }
+    int i;
+    int menor, maior;
+    // printf("Informe quantos numeros deseja passar: ");
+    scanf("%d",&i); // Armazena o tamanho do vetor.
+    int vet[i];
 
-   // SAIDA
-   printf("MENOR = %i\nMAIOR = %i", menor, maior);
-   return 0;
+    for(int j=0; j<i;j++){
+        // printf("Informe um numero: ");
+        scanf("%d",&vet[j]);
+    }
+
+    menor = vet[i];
+    maior = vet[0];
+
+    for(int j = 0; j < i; j++){
+        if(vet[j]<menor){
+            menor = vet[j];
+        }
+        if(vet[j]>=maior){
+            maior = vet[j];
+        }
+    }
+    printf("MENOR = %d\n", menor);
+    printf("MAIOR = %d", maior);
 }
