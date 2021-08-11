760f2d21940afe750d932c34a30f00f7000f3606
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-11 20:39:32 -0300

    Exercícios + testes

--- exe09/main.c
+++ exe09/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-
+/*
+Programa feito para calcular a media das notas dos alunos e informar as 3 maiores notas em ordem alfabética.
+Entrada: numero de alunos que deseja informar, seus nomes e suas notas.
+Saida: A media das notas dos alunos, as 3 maiores notas em ordem alfabética.
+*/
 struct aluno {
    float nota;
    char nome[50];
 };
 
+/*
+Procedimento feito para colocar as 3 maiores notas em ordem alfabética.
+Entrada: struct dos alunos e o tamanho da sala.
+Saída: As 3 maiores notas em ordem alfabética
+*/
 void ordemAlfabetica(struct aluno Sala[], int N){
    if(strcmp(Sala[N-3].nome, Sala[N-2].nome) > 0 && strcmp(Sala[N-1].nome, Sala[N-2].nome)> 0){
       printf("%s %.1f\n", Sala[N - 2].nome, Sala[N - 2].nota);
@@ -50,7 +59,7 @@ int main() {
    //printf("Quantos alunos deseja registrar?");
    scanf("%d",&qntdAlunos);
    
-   struct aluno Aluno[qntdAlunos];
+   struct aluno Aluno[qntdAlunos]; // Criação da struct com o tamanho da sala.
 
    for(int i=0; i<qntdAlunos; i++){
       //printf("Informe o nome do aluno: ");
@@ -59,7 +68,7 @@ int main() {
       notaTotal += Aluno[i].nota;
    }
 
-   for(int j=0; j<qntdAlunos; j++){
+   for(int j=0; j<qntdAlunos; j++){ // Repetição para definir as 3 maiores notas.
       for(int x = j; x <qntdAlunos; x++){
          if(Aluno[j].nota > Aluno[x].nota){
             aux = Aluno[j].nota;
