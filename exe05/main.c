10c04505fd8b17ac13de915f22e710a66e770f53
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-08-20 19:15:57 -0300

    Exercícios + testes

--- exe05/main.c
+++ exe05/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
 Programa feito para calcular a media de 4 notas e falar caso o aluno seja aprovado ou nao.
 Entrada: 4 notas.
@@ -7,13 +8,13 @@ Saida: Informa se o aluno foi aprovado caso sua media seja maior ou igual a 6.
 int main(){
 
     int n1, n2, n3, n4;
-    float soma=0;
+    int soma=0;
     float media;
 
     // printf("Informe sua nota: ");
     scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
     soma = n1 + n2 + n3 + n4;
-    media = soma/4.0;
+    media = (soma*1.0)/4;
 
     if(media>=6){
         printf("NOTA = %.1f (APROVADO)",media);
@@ -21,4 +22,5 @@ int main(){
     else{
         printf("NOTA = %.1f (REPROVADO)", media);
     }
+    return 0;
 }
